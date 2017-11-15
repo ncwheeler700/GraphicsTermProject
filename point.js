67315c3fadf7e2da37a3403cd7efc3eef9312550
Nate Wheeler <ncwheeler700@gmail.com>
2017-11-15 15:23:42 -0500

    Init commit

---
+++ point.js
@@ -0,0 +1,12 @@
+class Point {
+  constructor(pos,vel) {
+    this.pos = pos;
+    this.vel = vel;
+  }
+
+  getPos() {return pos;}
+  getVel() {return vel;}
+
+  setPos(newPos) {this.pos = newPos;}
+  setVel(newVel) {this.vel = newVel;}
+}
