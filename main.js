2ab74c5cb5a2ba484a91946a5b9e2b4faf444c7f
wrighl <wrighl@rpi.edu>
2017-12-07 19:47:06 -0500

    Vertex picking and manipulation

--- main.js
+++ main.js
@@ -48,21 +48,80 @@ window.onload = function init()
   gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
   gl.enableVertexAttribArray( vPosition );
 
+  var scale = 15.0;
+  var scale_uniform = gl.getUniformLocation( program, "scale" );
+  gl.uniform1f(scale_uniform, scale);
+    
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.drawElements(renderMode,indices.length,gl.UNSIGNED_SHORT,indexBuffer)
 
+    var mousedown = false;
+    var nearest = 0;
+    var mouse_x = 0;
+    var mouse_y = 0;
+
+    document.onmousemove = function(event) {
+	mouse_x = event.x - 10 - (canvas.width/2);
+	mouse_y = -(event.y - 10 - (canvas.height/2));
+	mouse_x = mouse_x * scale / (canvas.width/2);
+	mouse_y = mouse_y * scale / (canvas.height/2);
+	if (mousedown == false) {
+	    nearest = nearest_point(mesh.positions, mouse_x, mouse_y);
+	    console.log(nearest);
+	}
+    }
+
+    document.onmousedown = function() {
+	mousedown = true;
+    }
+
+    document.onmouseup = function() {
+	mousedown = false;
+    }
+
+
+    
   var animate = function() {
     window.requestAnimationFrame(animate)
     for (var i=0;i<20;i++) {
       mesh.nextStep();
     }
+      if (mousedown == true) {
+	  mesh.positions[nearest][0] = mouse_x;
+	  mesh.positions[nearest][1] = mouse_y;
+      }
     refresh(mesh,indexBuffer)
   }
   animate()
+    
+  /* From my observation, it appears that the upper left corner of the canvas is at
+   * (10,10) rather than (0,0), so I'll have to account for that offset in the final calculation.
+   * Additionally, the world scale is defined in the shader 
+   */
 
+    
 }
 
+function distance(x,y,x2,y2) {
+    d1 = x - x2;
+    d2 = y - y2;
+    return Math.sqrt(d1*d1 + d2*d2);
+}
 
+function nearest_point(vertices, x, y) {
+
+    var near = 0;
+    min_dist = 100;
+    for (i = 0; i < vertices.length; i++) {
+	dist = distance(x, y, vertices[i][0], vertices[i][1])
+        if (dist < min_dist) {
+	    near = i;
+	    min_dist = dist;
+	}
+    }
+
+    return near;
+}
 
 refresh = function(mesh,indexBuffer) {
     gl.clear(gl.COLOR_BUFFER_BIT);
